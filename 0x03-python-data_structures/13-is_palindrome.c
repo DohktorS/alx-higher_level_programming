b240350fdf8d73ee17ddcf1038998e4f1a076bf0
DohktorS <somtosherill66@gmail.com>
2023-03-14 14:43:25 -0700

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * reverse - Fx that reverses the second half of the list
+ * reverse - reverses the second half of the list
  *
  * @h_r: head of the second half
  * Return: no return
@@ -27,7 +27,7 @@ void reverse(listint_t **h_r)
 }
 
 /**
- * compare - fx that compares each int of the list
+ * compare - compares each int of the list
  *
  * @h1: head of the first half
  * @h2: head of the second half
@@ -43,7 +43,7 @@ int compare(listint_t *h1, listint_t *h2)
 
 	while (tmp1 != NULL && tmp2 != NULL)
 	{
-		if (tmp->n == tmp2->n)
+		if (tmp1->n == tmp2->n)
 		{
 			tmp1 = tmp1->next;
 			tmp2 = tmp2->next;
@@ -67,7 +67,7 @@ int compare(listint_t *h1, listint_t *h2)
  * is a palindrome
  * @head: pointer to head of list
  * Return: 0 if it is not a palindrome,
- * 1 if it is a palindrome
+ * 1 if it is a palndrome
  */
 int is_palindrome(listint_t **head)
 {
