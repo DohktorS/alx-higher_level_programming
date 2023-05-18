a21761cdab34c9a924c85b780007790b8b8855a2
DohktorS <somtosherill66@gmail.com>
2023-05-18 15:23:45 -0700

    Tasks 7 to 21 and advanced

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
--- lists all databases of your MySQL server.
-=======
--- list all databases of your MySQL server.
->>>>>>> 3c446d4da6e7510a7e1b481d54558068c3c2a4df
+-- script that lists all databases of your MySQL server
 SHOW DATABASES;
--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
--- creates the database hbtn_0c_0 in your MySQL server.
-=======
---creates the database hbtn_0c_0 in your MySQL server.
->>>>>>> 3c446d4da6e7510a7e1b481d54558068c3c2a4df
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- script that creates the database hbtn_0c_0 in your MySQL server
+-- If the database hbtn_0c_0 already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
